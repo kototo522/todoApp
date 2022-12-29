32191e4e9607dddfa886dc58dfaaa91f5dc8aa21
kototo522 <huyejiben@gmail.com>
2022-12-30 00:56:20 +0900

    add: ファイル名の変更(2回目)

--- app/src/main/java/com/example/todoapp/InputFragment.kt
+++ app/src/main/java/com/example/todoapp/InputFragment.kt
@@ -27,7 +27,7 @@ class InputFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imput, container, false)
+        return inflater.inflate(R.layout.fragment_input, container, false)
     }
 
     companion object {
