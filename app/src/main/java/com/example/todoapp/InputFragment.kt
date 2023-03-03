28af463fe415b3ad9c064254bf51b94e35fc495c
kototo522 <huyejiben@gmail.com>
2023-03-03 17:43:43 +0900

    おそらくbindingが機能してない

--- app/src/main/java/com/example/todoapp/InputFragment.kt
+++ app/src/main/java/com/example/todoapp/InputFragment.kt
@@ -1,45 +1,43 @@
 package com.example.todoapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import com.example.todoapp.databinding.FragmentInputBinding
 
-// TODO: Rename parameter arguments, choose names that match
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class InputFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentInputBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding.textView.text = "あいうえお"
+
+        binding.editButton.setOnClickListener {
+            // エディットテキストのテキストを取得
+            println("push")
+            if(binding.editText.text != null){
+                println("push")
+                Toast.makeText(requireContext(), binding.editText.text.toString(), Toast.LENGTH_SHORT).show()
+                binding.textView.text = binding.editText.text.toString()
+            }
         }
-        }
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+        _binding = FragmentInputBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InputFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
