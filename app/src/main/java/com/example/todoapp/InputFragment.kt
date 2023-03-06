537e12f048ca8a9e3a3eb41928d1dcbfb0a96999
kototo522 <huyejiben@gmail.com>
2023-03-07 00:17:33 +0900

    add: onDestroyViewの追加

--- app/src/main/java/com/example/todoapp/InputFragment.kt
+++ app/src/main/java/com/example/todoapp/InputFragment.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import android.widget.Toast
 import com.example.todoapp.databinding.FragmentInputBinding
@@ -16,21 +19,6 @@ class InputFragment : Fragment() {
     private var _binding: FragmentInputBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.textView.text = "あいうえお"
-
-        binding.editButton.setOnClickListener {
-            // エディットテキストのテキストを取得
-            println("push")
-            if(binding.editText.text != null){
-                println("push")
-                Toast.makeText(requireContext(), binding.editText.text.toString(), Toast.LENGTH_SHORT).show()
-                binding.textView.text = binding.editText.text.toString()
-            }
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,6 +26,21 @@ class InputFragment : Fragment() {
     ): View? {
         _binding = FragmentInputBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editButton.setOnClickListener {
+            if(binding.editText.text != null){
+                Toast.makeText(requireContext(), binding.editText.text.toString(), Toast.LENGTH_SHORT).show()
+                binding.textView.text = binding.editText.text.toString()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
