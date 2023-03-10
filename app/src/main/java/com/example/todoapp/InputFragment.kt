c7614312a24ad7187df461a10ebed5cdbc4cc7a7
kototo522 <huyejiben@gmail.com>
2023-03-10 16:35:34 +0900

    デバッグに使ったコードを削除

--- app/src/main/java/com/example/todoapp/InputFragment.kt
+++ app/src/main/java/com/example/todoapp/InputFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import android.widget.Toast
 import com.example.todoapp.databinding.FragmentInputBinding
 import android.widget.LinearLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -18,8 +16,6 @@ private const val ARG_PARAM2 = "param2"
 class ModalBottomSheet : BottomSheetDialogFragment() {
     private lateinit var bottomSheet: View
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
-
-
     private var _binding: FragmentInputBinding? = null
     private val binding get() = _binding!!
 
@@ -36,16 +32,13 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         bottomSheet = view.findViewById(R.id.bottom_sheet_layout)
         bottomSheet.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
-
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-
         val screenHeight = Resources.getSystem().displayMetrics.heightPixels
         bottomSheetBehavior.peekHeight = screenHeight
 
         binding.editButton.setOnClickListener {
             if(binding.editText.text != null){
-                Toast.makeText(requireContext(), binding.editText.text.toString(), Toast.LENGTH_SHORT).show()
                 binding.textView.text = binding.editText.text.toString()
             }
         }
