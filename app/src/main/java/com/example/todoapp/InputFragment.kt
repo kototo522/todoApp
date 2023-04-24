7462af493a104273a621f4339a2281d03a2fd0db
kototo522 <huyejiben@gmail.com>
2023-04-24 18:54:54 +0900

    buttonActionの追加

--- app/src/main/java/com/example/todoapp/InputFragment.kt
+++ app/src/main/java/com/example/todoapp/InputFragment.kt
@@ -5,16 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import com.example.todoapp.databinding.FragmentInputBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ModalBottomSheet : BottomSheetDialogFragment() {
-    private lateinit var bottomSheet: View
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
     private var _binding: FragmentInputBinding? = null
     private val binding get() = _binding!!
@@ -23,23 +19,25 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentInputBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bottomSheet = view.findViewById(R.id.bottom_sheet_layout)
+        val bottomSheet: View = view.findViewById(R.id.bottom_sheet_layout)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-        bottomSheet.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
-        bottomSheetBehavior.saveFlags = BottomSheetBehavior.SAVE_ALL
         val screenHeight = Resources.getSystem().displayMetrics.heightPixels
         bottomSheetBehavior.peekHeight = screenHeight
 
         binding.editButton.setOnClickListener {
             if(binding.editTitle.text != null) binding.titleView.text = binding.editTitle.text.toString()
             if(binding.editText.text != null) binding.textView.text = binding.editText.text.toString()
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+        binding.exitButton.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
 
         // もっとシンプルにできそう
